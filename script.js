fe0f63ad3f43d0ee1e03d42f2aac462d454d900b
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-24 04:11:30 -0700

    add confirm pop ups for password criteria confirmation

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-// Variables for the prompt confirmations
+// Undefined variables for the prompt confirmations
 var len;
-var 
+var useLowercase;
+var useUppercase;
+var useNumeric;
+var useSpecialChar;
 
 // Write password to the #password input
 function writePassword() {
@@ -20,7 +23,14 @@ function writePassword() {
 
 // Function to generate password given user inputs
 function generatePassword() {
+  // Ask user for desired length of password
   var len = prompt("How long would you like your password to be (between 8 to 128 characters)?");
+  // Ask user to confirm if they want these certain criteria
+  var useLowercase = confirm("Confirm if you want lowercase characters in your password.");
+  var useUppercase = confirm("Confirm if you want uppercase characters in your password.");
+  var useNumeric = confirm("Confirm if you want numeric characters in your password.");
+  var useSpecialChar = confirm("Confirm if you want special characters in your password.");
+
 
 }
 
