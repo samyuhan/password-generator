9591b7a4c807da161d41de9767eec46d6b279750
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-24 03:54:25 -0700

    add arrays for different types of characters

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Arrays for the different types of characters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+// Variables for the prompt confirmations
+var len;
+var 
 
 // Write password to the #password input
 function writePassword() {
@@ -7,6 +15,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+// Function to generate password given user inputs
+function generatePassword() {
+  var len = prompt("How long would you like your password to be (between 8 to 128 characters)?");
 
 }
 
