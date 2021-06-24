27107195d04e7f810807761a12084ef1bb3f2283
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-24 04:23:12 -0700

    add do while loop to make sure at least one criteria is confirmed

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var passwordCombos = [];
+var generatedPassword = "";
 // Undefined variables for the prompt confirmations
 var len;
 var useLowercase;
@@ -25,13 +27,23 @@ function writePassword() {
 function generatePassword() {
   // Ask user for desired length of password
   var len = prompt("How long would you like your password to be (between 8 to 128 characters)?");
-  // Ask user to confirm if they want these certain criteria
-  var useLowercase = confirm("Confirm if you want lowercase characters in your password.");
-  var useUppercase = confirm("Confirm if you want uppercase characters in your password.");
-  var useNumeric = confirm("Confirm if you want numeric characters in your password.");
-  var useSpecialChar = confirm("Confirm if you want special characters in your password.");
+  // Add a do while loop to make sure at least one criteria is confirmed
+  do {
+    // Ask user to confirm if they want these certain criteria
+    var useLowercase = confirm("Confirm if you want lowercase characters in your password.");
+    var useUppercase = confirm("Confirm if you want uppercase characters in your password.");
+    var useNumeric = confirm("Confirm if you want numeric characters in your password.");
+    var useSpecialChar = confirm("Confirm if you want special characters in your password.");
+  } while (useLowercase || useUppercase || useNumeric || useSpecialChar)
+  
 
+  
+  // Checks to see if lowercase was confirmed, then it'll add it to the password combinations list
+  if(useLowercase) {
+  }
 
+  // Returns the randomly generated password that was created
+  return generatedPassword;
 }
 
 // Add event listener to generate button
