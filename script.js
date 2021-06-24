91627b9af6d33fdd959969d5ee5c0b08678b9129
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-24 04:35:28 -0700

    add if statements to concat specific criteria

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var passwordCombos = [];
 var generatedPassword = "";
 // Undefined variables for the prompt confirmations
@@ -25,23 +25,36 @@ function writePassword() {
 
 // Function to generate password given user inputs
 function generatePassword() {
-  // Ask user for desired length of password
-  var len = prompt("How long would you like your password to be (between 8 to 128 characters)?");
-  // Add a do while loop to make sure at least one criteria is confirmed
+  // Ask user for desired length of password and convert to integer
+  var len = parseInt(prompt("How long would you like your password to be (between 8 to 128 characters)?"));
+  // Add a do while loop to make sure at least one criteria is confirmed at the end
   do {
     // Ask user to confirm if they want these certain criteria
     var useLowercase = confirm("Confirm if you want lowercase characters in your password.");
     var useUppercase = confirm("Confirm if you want uppercase characters in your password.");
     var useNumeric = confirm("Confirm if you want numeric characters in your password.");
     var useSpecialChar = confirm("Confirm if you want special characters in your password.");
-  } while (useLowercase || useUppercase || useNumeric || useSpecialChar)
-  
-
+    alert("If you didn't choose at least one criteria, you will be prompted to confirm again.")
+  } while (!useLowercase || !useUppercase || !useNumeric || !useSpecialChar)
   
   // Checks to see if lowercase was confirmed, then it'll add it to the password combinations list
   if(useLowercase) {
+    passwordCombos = passwordCombos.concat(lowercase);
+  }
+  // Checks to see if uppercase was confirmed, then it'll add it to the password combinations list
+  if(useUppercase) {
+    passwordCombos = passwordCombos.concat(uppercase);
+  }
+  // Checks to see if numeric was confirmed, then it'll add it to the password combinations list
+  if(useNumeric) {
+    passwordCombos = passwordCombos.concat(numeric);
+  }
+  // Checks to see if special character was confirmed, then it'll add it to the password combinations list
+  if(useSpecialChar) {
+    passwordCombos = passwordCombos.concat(specialChar);
   }
 
+  console.log(passwordCombos);
   // Returns the randomly generated password that was created
   return generatedPassword;
 }
